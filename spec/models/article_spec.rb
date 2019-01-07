ff7622e0abc40db14d340c949b0147b81d4066e0
Abhishek Sah <abhi.sah.97@gmail.com>
2019-01-07 12:12:50 +0530

    [Abhishek] Tests article model to not save articles with no title

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe Article do
+    it 'should not save article without title' do
+      article = Article.new
+      expect(article.save).to be(false)
+    end
+  end
 end
