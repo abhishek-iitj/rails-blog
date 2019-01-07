21c5a04c0591a9ae05160f846dcf0e5fa3a72f4c
Abhishek Sah <abhi.sah.97@gmail.com>
2019-01-07 13:09:30 +0530

    [Abhishek] Refactors validation tests on article model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Article, type: :model do
   describe Article do
-    it 'should not save article without title' do
+    it 'is invalid without title' do
       article = Article.new
-      expect(article.save).to be(false)
+      expect(article.valid?).to be(false)
     end
     it 'is invalid with title length less than 5' do
       article = Article.new(title: 'some')
-      expect(article.save).to be(false)
+      expect(article.valid?).to be(false)
     end
   end
 end
