ef1ae7c6c446598d8090c7ea60e1dc97559c519f
Abhishek Sah <abhi.sah.97@gmail.com>
2019-01-12 15:55:12 +0530

    [Abhishek] Adds test for ArticlesController#index rendered view

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe ArticlesController, type: :controller do
-
+  describe 'Articles' do
+    describe 'GET #index' do
+      it 'renders the :index view' do
+        get :index
+        expect(response).to render_template(:index)
+      end
+    end
+  end
 end
